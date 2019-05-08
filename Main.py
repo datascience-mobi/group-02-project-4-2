108bc309b4db59aaed070531a57c0fa784d48a97
tristan-ranff <49940146+tristan-ranff@users.noreply.github.com>
2019-05-08 02:52:45 +0200

    Added time measuring

--- Main.py
+++ Main.py
@@ -4,21 +4,37 @@ import pandas
 import matplotlib.pyplot as pyplot
 import matplotlib.cm as cm
 import scanpy as sc
+from datetime import datetime
 
+# Global Variables
+t1 = 0
+
+# Functions
 def dist(patient_point, cluster_number):
     a = filtered_data[patient_point, :]
     b = centroids_array[cluster_number, :]
     dist = np.linalg.norm(a-b)
     return dist
 
+def runtime_start():
+    global t1 
+    t1 = datetime.now().time()
+
+def runtime_end():
+    t2 = datetime.now().time()
+    FMT = '%H:%M:%S.%f'
+    elapsed = str(datetime.strptime(str(t2), FMT) - datetime.strptime(str(t1), FMT))
+    return elapsed
+
 # Import data
+runtime_start()
 data = sc.read_10x_mtx('./data/filtered_gene_bc_matrices/hg19/', var_names='gene_symbols', cache=True)
 
-#Filter useless data
+# Filter useless data
 sc.pp.filter_genes(data, min_cells=1)
 filtered_data = np.array(data._X.todense())
 
-#Create Centroid Array
+# Create Centroid Array
 clusters_amount = 5
 centroids_numbers = np.random.randint(2700, size=clusters_amount)
 centroids_array = np.empty([0, 16634])
@@ -27,3 +43,4 @@ while i < clusters_amount:
     randompatient = centroids_numbers[i]
     centroids_array = np.append(centroids_array, [filtered_data[randompatient, :]], axis = 0)
     i += 1
+runtime_end()
