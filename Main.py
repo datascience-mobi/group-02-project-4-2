a8f00b3f0e516c0af7773ddbb6d1bd8808d48ed1
BastianEichmueller <49943617+BastianEichmueller@users.noreply.github.com>
2019-05-10 19:18:01 +0200

    Minor adjustments (removing useless bits and PEP8 check)
    else pass is not necessary,
    global keyword is not needed to read a global variable inside a function (only to change it)

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ import matplotlib.cm as cm
 import scanpy as sc
 from datetime import datetime
 from sklearn.decomposition import PCA
-from sklearn.cluster import KMeans 
+from sklearn.cluster import KMeans
 
 
 # Global Variables
@@ -21,7 +21,7 @@ k = 0
 # Functions
 # Define distance function which takes integer inputs which identify patient and centroid
 def runtime_start():
-    global t1 
+    global t1
     t1 = datetime.now().time()
 
 
@@ -29,12 +29,12 @@ def runtime_end():
     t2 = datetime.now().time()
     fmt = '%H:%M:%S.%f'
     elapsed = str(datetime.strptime(str(t2), fmt) - datetime.strptime(str(t1), fmt))
-    return str("Runtime: " + elapsed)
+    return str("runtime: " + elapsed)
 
 
 def random_start_centroids(starttype):
     # Create Centroid Array by randomly picking k patients from data
-    global centroids_array, patients, genes, k
+    global centroids_array, patients, genes
     patients = pca_data.shape[0]
     genes = pca_data.shape[1]
     centroids_array = np.empty([0, genes])
@@ -49,14 +49,16 @@ def random_start_centroids(starttype):
             i += 1
 
     elif starttype == "randnum":
-        centroids_array = (np.amax(pca_data) - np.amin(pca_data))* np.random.random_sample((k, genes)) + np.amin(pca_data)
+        centroids_array = (np.amax(pca_data) - np.amin(pca_data)) * np.random.random_sample((k, genes)) + np.amin(
+            pca_data)
+
 
 def assign_centroids():
-    global nearest_centroid, patients, k
+    global nearest_centroid
     # Assign closest Centroid
     # Loop über alle Punkte
     i = 0
-    nearest_centroid = np.zeros([patients, 1])    
+    nearest_centroid = np.zeros([patients, 1])
     while i < patients:
         sml_distance = 0
 
@@ -70,71 +72,68 @@ def assign_centroids():
             j += 1
         i += 1
 
+
 def empty_check():
     # Sicherstellen dass es durch die Zufallscentroids keine leeren Cluster gibt
     i = 0
     while i < k:
-        if list(nearest_centroid).count(i+1) == 0:
+        if list(nearest_centroid).count(i + 1) == 0:
             print("Empty cluster! Correcting centroids.")
             random_start_centroids("randnum")
             assign_centroids()
             empty_check()
-        else:
-            pass
         i += 1
 
+
 def dist(patient_point, cluster_number):
-    global centroids_array
     a = pca_data[patient_point, :]
-    b = centroids_array[cluster_number-1, :]
-    d = np.linalg.norm(a-b)
+    b = centroids_array[cluster_number - 1, :]
+    d = np.linalg.norm(a - b)
     return d
-    
+
 
 def new_centroids():
     global centroids_array
-    zeros = np.zeros([patients,1])
+    zeros = np.zeros([patients, 1])
     centroids_array = np.empty([0, genes])
-    #"Masken" um values aus pca_data abzurufen
+    # "Masken" um values aus pca_data abzurufen
     nearest_centroidpca1 = np.append(nearest_centroid, zeros, axis=1)
     nearest_centroidpca2 = np.append(zeros, nearest_centroid, axis=1)
-    #while loop der für alle k cluster läuft:
+    # while loop der für alle k cluster läuft:
     i = 1
     while i <= k:
         pca1 = np.mean(pca_data[nearest_centroidpca1 == i])
         pca2 = np.mean(pca_data[nearest_centroidpca2 == i])
         centroids_array = np.append(centroids_array, [[pca1, pca2]], axis=0)
         i += 1
-              
+
 
 def kmeans(k1, n_iterations):
-        global k
-        k = k1
-        i = 0
-        random_start_centroids("randnum")
+    global k
+    k = k1
+    i = 0
+    random_start_centroids("randnum")
+    assign_centroids()
+    empty_check()
+    while i < n_iterations:
+        new_centroids()
         assign_centroids()
-        empty_check()
-        while i<n_iterations:
-                new_centroids()
-                assign_centroids()
-                i+=1
+        i += 1
 
 
 # General Code
 # Import data
 data = sc.read_10x_mtx('./data/filtered_gene_bc_matrices/hg19/', var_names='gene_symbols', cache=True)
 
-
 # Filter useless data
 sc.pp.filter_genes(data, min_cells=1)
 filtered_data = np.array(data._X.todense())
 
-
 # PCA
 pca = PCA(n_components=2)
 pca_data = pca.fit_transform(filtered_data)
-#print(sum(pca.explained_variance_ratio_))
-#print(pca.singular_values_)
+# print(sum(pca.explained_variance_ratio_))
+# print(pca.singular_values_)
 
 
 # Execute
@@ -144,16 +143,15 @@ print("\nkmeans:")
 print(runtime_end())
 
 # plotting
-fig = pyplot.figure(1, figsize=[10,5], dpi = 200)
-plt1, plt2 = fig.subplots(1,2, sharex=True, sharey=True)
+fig = pyplot.figure(1, figsize=[10, 5], dpi=200)
+plt1, plt2 = fig.subplots(1, 2, sharex=True, sharey=True)
 nearest_centroid_squeeze = np.squeeze(nearest_centroid.astype(int))
 plt1.scatter(pca_data[:, 0], pca_data[:, 1], c=nearest_centroid_squeeze, s=20, cmap='viridis')
 plt1.set_title('kmeans')
 a_str = np.array2string(centroids_array, precision=2, separator=' ')
 print("centroids: \n" + ' ' + a_str[1:-1])
 
-
-#sklearn comparison
+# sklearn comparison
 print("\nsklearn kmeans:")
 runtime_start()
 sklearn_kmeans = KMeans(n_clusters=k).fit(pca_data)
